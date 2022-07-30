334b2712e49c6d82f6e482211af573d7a9ee3360
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-07-30 07:29:23 -0400

    changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import session from 'express-session';
 import helmet from 'helmet';
 import config from './config/config.js';
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 4200
 
 app.use(helmet());
 app.use(cors());
