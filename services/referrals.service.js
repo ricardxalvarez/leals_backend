6d5fce47020e4534dbea57fd5b8a1f0cb3e09cd4
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-07-31 10:15:33 -0400

    changes

--- services/referrals.service.js
+++ services/referrals.service.js
@@ -7,6 +7,7 @@ export async function searchReferral(text, id_sponsor) {
 
 export async function referralChildren({ iduser, level }) {
   let users = await (await conexion.query("SELECT id, nombre_usuario, avatar, id_sponsor, avatar FROM usuarios WHERE id_progenitor=($1) OR id=($1)", [iduser])).rows
+  console.log(users);
   function Node(user) {
     this.user = user,
       this.children = [];
