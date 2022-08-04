c5b12e434cf1cbf76fba0837c739ff4594623421
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-04 16:09:07 -0400

    changes

--- services/referrals.service.js
+++ services/referrals.service.js
@@ -1,4 +1,5 @@
 import conexion from '../database/conexion.js'
+import resizeImageBase64 from '../utils/resizeImageBase64.js';
 
 export async function searchReferral(text, iduser, id) {
   const users = await (await conexion.query("SELECT id, nombre_usuario, avatar, id_sponsor, avatar, codigo_pais FROM usuarios WHERE id_progenitor = ($1) OR id = ($1) ORDER BY id_sponsor NULLS FIRST", [iduser])).rows
@@ -96,7 +97,7 @@ export async function searchReferral(text, iduser, id) {
 
 export async function referralChildren({ iduser, level, id }) {
   let users = await (await conexion.query("SELECT id, nombre_usuario, avatar, id_sponsor, avatar, codigo_pais FROM usuarios WHERE id_progenitor=($1) OR id=($1) ORDER BY id_sponsor NULLS FIRST", [iduser])).rows
-  // users.map(object => {return {...object, avatar}})
+  users.map(object => { return { ...object, avatar: resizeImageBase64(100, 100, 60, object.avatar) } })
   function Node(user) {
     this.user = user,
       this.children = [];
---
+++ utils/resizeImageBase64.js
@@ -0,0 +1,21 @@
+import jimp from 'jimp'
+
+async function resizeImageBase64(width, height, quality, image) {
+    if (!image) return null
+    const base = 'base64'
+    const base64str = image.slice(image.indexOf(base) + base.length + 1)
+    const buf = Buffer.from(base64str, base);
+    jimp.read(buf, (err, image) => {
+        if (err) throw err;
+        else {
+            image.resize(width, height, jimp.AUTO)
+                .quality(quality)
+                .getBase64(jimp.MIME_JPEG, function (err, src) {
+                    if (err) return null
+                    return src
+                })
+        }
+    })
+}
+
+export default resizeImageBase64
