3ee52d8b764fa41b53a5055d6102bfd5cad8ef90
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-04 16:11:17 -0400

    changes

--- utils/resizeImageBase64.js
+++ utils/resizeImageBase64.js
@@ -2,11 +2,12 @@ import jimp from 'jimp'
 
 async function resizeImageBase64(width, height, quality, image) {
     if (!image) return null
+    console.log(image)
     const base = 'base64'
     const base64str = image.slice(image.indexOf(base) + base.length + 1)
     const buf = Buffer.from(base64str, base);
     jimp.read(buf, (err, image) => {
-        if (err) throw err;
+        if (err) return null;
         else {
             image.resize(width, height, jimp.AUTO)
                 .quality(quality)
