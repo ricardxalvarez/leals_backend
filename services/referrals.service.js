d201e46e05f77e609d942379a9ab996167bb205c
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-04 09:36:48 -0400

    changes

--- services/referrals.service.js
+++ services/referrals.service.js
@@ -85,7 +85,8 @@ export async function searchReferral(text, iduser, id) {
     childsCount.push(element.length)
   }
   if (text) {
-    let usersList = results.filter(object => object.user.nombre_usuario.toLowerCase().includes(text.toLowerCase())).sort((a, b) => a.user.nombre_usuario - b.user.nombre_usuario)//.splice(results.map(function (e) { return e.user.nombre_usuario; }).indexOf(text), 0, results.splice(0, 1)[0])
+    const matchingElement = results.find(object => object.user.nombre_usuario === text)
+    let usersList = results.filter(object => object.user.nombre_usuario.toLowerCase().includes(text.toLowerCase())).sort((a, b) => a.user.nombre_usuario - b.user.nombre_usuario).filter(object => object.user.nombre_usuario === text).unshift(matchingElement)
     results = { user: results[0].user, children: usersList.length > 0 ? usersList : 'No user found' }
   } else results[0]
   return { results, last_level: lastLevel, childs_count: childsCount }
