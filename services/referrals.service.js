0b7aad30acdbfd67383df1864bab0803551ca4ff
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-04 16:40:22 -0400

    changes

--- services/referrals.service.js
+++ services/referrals.service.js
@@ -97,8 +97,8 @@ export async function searchReferral(text, iduser, id) {
 
 export async function referralChildren({ iduser, level, id }) {
   let users = await (await conexion.query("SELECT id, nombre_usuario, avatar, id_sponsor, avatar, codigo_pais FROM usuarios WHERE id_progenitor=($1) OR id=($1) ORDER BY id_sponsor NULLS FIRST", [iduser])).rows
-  users = users.map(async object => {
-    const newAvatar = await resizeImageBase64(100, 100, 60, object.avatar)
+  users = users.map(object => {
+    const newAvatar = resizeImageBase64(100, 100, 60, object.avatar)
     return { ...object, avatar: newAvatar }
   })
   function Node(user) {
