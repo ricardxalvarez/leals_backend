4417cdcc99ed1acd6aaad45b79fbcfee54881625
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-04 14:11:56 -0400

    changes

--- middlewares/validateAvatarURL.js
+++ middlewares/validateAvatarURL.js
@@ -6,29 +6,18 @@ function validateAvatarURL(req, res, next) {
     if (!data) {
         return next()
     }
-    let knownTypes = {
-        '/': 'data:image/jpg;base64,',
-        'i': 'data:image/png;base64,',
-        /*ETC*/
-    }
 
     let image = new Image()
-    console.log(knownTypes[data[0]])
-    if (!knownTypes[data[0]]) {
-        console.log("encoded image didn't match known types");
-        return res.send({ status: false, content: "encoded image didn't match known types" });
-    } else {
-        image.src = knownTypes[0] + data
-        image.onload = function () {
-            //This should load the image so that you can actually check
-            //height and width.
-            if (image.height === 0 || image.width === 0) {
-                console.log('encoded image missing width or height');
-                return res.send({ status: false, content: "encoded image missing width or height" });
-            }
+    image.src = data
+    image.onload = function () {
+        //This should load the image so that you can actually check
+        //height and width.
+        if (image.height === 0 || image.width === 0) {
+            console.log('encoded image missing width or height');
+            return res.send({ status: false, content: "encoded image missing width or height" });
         }
-        return next();
     }
+    return next();
 }
 
 export default validateAvatarURL
