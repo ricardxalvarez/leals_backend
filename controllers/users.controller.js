7c5f3f8802972eb64f96708c02dfae6549f7b147
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-05 16:43:35 -0400

    changes

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -493,7 +493,7 @@ async function sendMailToClient(mailOptions) {
 async function sendMessageToClient(phone, code) {
   client.verify.v2.services('VA8fae577af25bf454192050a0c45af96d')
     .verifications
-    .create({ to: phone, channel: 'whatsapp', customFriendlyName: 'Leals team', customMessage: 'hola' })
+    .create({ customCode: code, to: phone, channel: 'whatsapp', customFriendlyName: 'Leals team' })
     .then(response => console.log(response))
     .catch(error => console.log(error))
 }
