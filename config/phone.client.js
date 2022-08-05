8e24358dc4b9fbbefa8d16a9345d153520bc68b7
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-05 15:45:46 -0400

    changes

--- config/phone.client.js
+++ config/phone.client.js
@@ -4,4 +4,9 @@ const accountSid = config.twilio.account_sid;
 const authToken = config.twilio.auth_token;
 const client = twilio(accountSid, authToken)
 
+client.verify.v2.services('VA8fae577af25bf454192050a0c45af96d')
+    .verifications
+    .create({ to: config.twilio.phone_number, channel: 'whatsapp' })
+    .then(verification => console.log(verification.accountSid));
+
 export default client
