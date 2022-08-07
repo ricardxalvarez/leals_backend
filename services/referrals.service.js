88bbcad6b343581371c4c8621bbbf492110526f5
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-07 10:20:42 -0400

    changes

--- services/referrals.service.js
+++ services/referrals.service.js
@@ -127,10 +127,10 @@ export async function referralChildren({ iduser, level, id }) {
           if (parent.children[parent.children.length - 1].user.id_sponsor === node.user.id_sponsor) {
             parent.children.push({ ...node, user: { ...node.user, level: this.level } })
           } else {
+            this.level++;
             parent.children.push({ ...node, user: { ...node.user, level: this.level } })
           }
         } else {
-          this.level++;
           parent.children.push({ ...node, user: { ...node.user, level: this.level } })
         }
       } else {
