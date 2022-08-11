c9756005dfbfe3981c55dc55d7efc4a8a9286b74
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-11 15:39:38 -0400

    changes

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -311,7 +311,7 @@ export function updateUser(req, res) {
   userService.checkEmailExists(email, emailUser)
     .then(response => {
       if (response.status) {
-        let newData = { ...response.myUser, ...values }
+        let newData = { ...response.myUser, fullname: response.myUser.full_nombre, skills: response.myUser.habilidades, ...values }
         userService.updateUser(newData, email, emailUser, phone, phoneUser)
           .then(user => {
             res.send(user)
