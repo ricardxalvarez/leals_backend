cb0c166552d29fff7df634341fd6fa43a85e4d49
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-23 14:37:03 -0400

    validate username v1.0.1

--- middlewares/validate.js
+++ middlewares/validate.js
@@ -1,7 +1,6 @@
 import joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 import pick from '../utils/pick.js'
-import ApiError from '../utils/ApiError.js'
 const validate = (schema) =>
     (req, res, next) => {
         const validSchema = pick(schema, ["params", "query", "body"])
@@ -14,8 +13,7 @@ const validate = (schema) =>
             const errorMessage = error.details
                 .map((details) => details.message)
                 .join(", ");
-            res.status(StatusCodes.BAD_REQUEST).send(errorMessage)
-            return new ApiError(StatusCodes.BAD_REQUEST, errorMessage);
+            return res.status(StatusCodes.BAD_REQUEST).send(errorMessage)
         }
         Object.assign(req, value);
         return next();
