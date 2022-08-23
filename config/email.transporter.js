544cbf605dd75fcf4decf202f487d22c4ea6a38d
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-23 14:32:23 -0400

    validate username v1.0

--- config/email.transporter.js
+++ config/email.transporter.js
@@ -12,8 +12,6 @@ const smptConfig = {
     secure: config.email.secure
 };
 
-console.log(smptConfig)
-
 let transporter = nodemailer.createTransport(smptConfig);
 
 transporter.verify(function (error, success) {
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -13,6 +13,7 @@ export function postSignup(req, res, next) {
   const { fullname, email, idcountry, username, password1, referralusername, phone } = req.body
   var re = /^ [a-z0-9]*$/
   if (!re.test(username)) {
+    console.log(re.test(username))
     return res.send({ status: false, content: 'Only lowercase characters and numbers are accepted as username' })
   }
   let salt = bcrypt.genSaltSync(10);
