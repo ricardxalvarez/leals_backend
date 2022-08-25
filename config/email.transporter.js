05e18eb92e621dad84e68ddc00ad47533ee869b0
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-08-24 20:32:48 -0400

    aws nodemailer config v1

--- config/email.transporter.js
+++ config/email.transporter.js
@@ -4,12 +4,10 @@ import config from './config.js'
 const smptConfig = {
     host: config.email.host,
     port: config.email.port,
-    service: config.email.service,
     auth: {
         user: config.email.auth.user,
         pass: config.email.auth.pass
-    },
-    secure: config.email.secure
+    }
 };
 
 let transporter = nodemailer.createTransport(smptConfig);
