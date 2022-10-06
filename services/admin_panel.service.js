25cb5b48a4094c451caa9a61b3477ee114fd73c1
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-10-06 00:51:16 -0400

    seconds stage p2p v1.1.3

--- services/admin_panel.service.js
+++ services/admin_panel.service.js
@@ -1,7 +1,7 @@
 import conexion from '../database/conexion.js'
 import create_wallet from '../utils/create_wallet.js'
 export async function add_balance(userid, amount) {
-    const wallet = await (await conexion.query('SELECT * FRPM wallets WHERE owner=($1)', [userid])).rows[0]
+    const wallet = await (await conexion.query('SELECT * FROM wallets WHERE owner=($1)', [userid])).rows[0]
     if (!wallet) await create_wallet(userid)
     const new_amount = wallet?.balance ? wallet.balance + amount : amount
     await conexion.query('UPDATE wallets SET balance=($1) WHERE owner=($2)', [new_amount, userid])
