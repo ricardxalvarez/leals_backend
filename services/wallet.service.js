fda27689e43572d181e615bfee0b8753e8c8a813
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-10-07 11:35:25 -0400

    seconds stage p2p v1.1.3

--- services/wallet.service.js
+++ services/wallet.service.js
@@ -6,6 +6,6 @@ export async function wallet_info(userid) {
     if (!wallet) return { ...await (await conexion.query('INSERT INTO wallets (owner) VALUES ($1) RETURNING *', [userid])).rows[0], leals_balance: "0.00" }
     const leals_balance = fix_number(wallet.balance / p2p_config.value_compared_usdt) || "0.00"
     wallet.balance = fix_number(wallet.balance) || "0.00"
-    wallet.not_available = fix_number(wallet.not_available * p2p_config.value_compared_usdt) || "0.00"
+    wallet.not_available = fix_number(wallet.not_available / p2p_config.value_compared_usdt) || "0.00"
     return { ...wallet, leals_balance }
 }
