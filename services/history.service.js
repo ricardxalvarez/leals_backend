2b5d3e400bafacce6076d7eb4a961d4538bc1b7d
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-10-10 20:34:41 -0400

    client controller v1

--- services/history.service.js
+++ services/history.service.js
@@ -1,6 +1,7 @@
 import conexion from '../database/conexion.js'
 
 export async function list_p2p(userid) {
-    const list = await (await conexion.query('SELECT * FROM history WHERE owner=($1)', [userid])).rows
+    const username = await (await conexion.query('SELECT nombre_usuario FROM usuarios WHERE id=($1)')).rows[0]?.nombre_usuario
+    const list = await (await conexion.query('SELECT * FROM history WHERE owner=($1) OR destinary_transfer=($2)', [userid, username])).rows
     return list
 }
