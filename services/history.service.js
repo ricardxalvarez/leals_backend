24d0782e4362ce684efec14141c8c9d8d9b93767
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-10-10 20:51:03 -0400

    second stage p2p v1.1.3

--- services/history.service.js
+++ services/history.service.js
@@ -1,7 +1,7 @@
 import conexion from '../database/conexion.js'
 
 export async function list_p2p(userid) {
-    const username = await (await conexion.query('SELECT nombre_usuario FROM usuarios WHERE id=($1)'), [userid]).rows[0]?.nombre_usuario
+    const username = await (await conexion.query('SELECT nombre_usuario FROM usuarios WHERE id=($1)', [userid])).rows[0]?.nombre_usuario
     const list = await (await conexion.query('SELECT * FROM history WHERE owner=($1) OR destinary_transfer=($2)', [userid, username])).rows
     return list
 }
