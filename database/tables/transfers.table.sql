93f09efbcaeee2227d89a441809a461a139ec16a
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-10-24 21:18:24 -0400

    second stage p2p v1.1.3

--- database/tables/transfers.table.sql
+++ database/tables/transfers.table.sql
@@ -2,4 +2,4 @@ CREATE TABLE transfers (
     owner BIGINT REFERENCES usuarios(id) NOT NULL,
     amount DOUBLE PRECISION CHECK(amount > 0),
     destinary BIGINT REFERENCES usuarios(id) NOT NULL
-)
+);
--- services/wallet.service.js
+++ services/wallet.service.js
@@ -5,7 +5,9 @@ export async function wallet_info(userid) {
     const p2p_config = await (await conexion.query('SELECT * FROM p2p_config')).rows[0]
     if (!wallet) return { ...await (await conexion.query('INSERT INTO wallets (owner) VALUES ($1) RETURNING *', [userid])).rows[0], leals_balance: "0.00" }
     const leals_balance = fix_number(wallet.balance / p2p_config.value_compared_usdt) || "0.00"
+    const not_available_leals_balance = fix_number(wallet.not_available / p2p_config.value_compared_usdt)
     wallet.balance = fix_number(wallet.balance) || "0.00"
-    wallet.not_available = fix_number(wallet.not_available / p2p_config.value_compared_usdt) || "0.00"
-    return { ...wallet, leals_balance }
+    wallet.not_available = fix_number(wallet.not_available) || "0.00"
+
+    return { ...wallet, leals_balance, not_available_leals_balance }
 }
