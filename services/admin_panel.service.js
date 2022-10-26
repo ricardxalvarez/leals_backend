e8e01451b9646e06184e054b5539e149500fe083
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-10-26 08:38:17 -0400

    second stage p2p v1.1.3

--- services/admin_panel.service.js
+++ services/admin_panel.service.js
@@ -2,9 +2,11 @@ import conexion from '../database/conexion.js'
 import create_wallet from '../utils/create_wallet.js'
 export async function add_balance(userid, amount) {
     const wallet = await (await conexion.query('SELECT * FROM wallets WHERE owner=($1)', [userid])).rows[0]
+    const p2p_config = await (await conexion.query('SELECT * FROM p2p_config')).rows[0]
     if (!wallet) await create_wallet(userid)
     const new_amount = wallet?.balance ? wallet.balance + amount : amount
     await conexion.query('UPDATE wallets SET balance=($1) WHERE owner=($2)', [new_amount, userid])
+    await conexion.query('INSERT INTO history (owner, history_type, cash_flow, amount, leals_amount, currency, date) VALUES($1,$2,$3,$4,$5,$6,$7)', [userid, 'balance add', 'income', amount, amount / p2p_config.value_compared_usdt, 'usdt', new Date()])
     return { status: true, content: 'Balance updated' }
 }
 
@@ -21,11 +23,13 @@ export async function clean() {
 }
 
 export async function approve_advertise(advertise_id) {
-    await conexion.query('UPDATE advertises SET status=($1) WHERE advertise_id=($2)', ['approved', advertise_id])
+    const ad = await (await conexion.query('UPDATE advertises SET status=($1) WHERE advertise_id=($2) RETURNING *', ['approved', advertise_id])).rows[0]
+    await conexion.query('INSERT INTO notifications (owner, message, date) VALUES($1,$2,$3)', [ad.owner, 'Excellent work, your ad was registered successfully', new Date()])
     return { status: true, content: 'Advertise succesfully approved' }
 }
 
 export async function deny_advertise(advertise_id) {
-    await conexion.query('UPDATE advertises SET status=($1) WHERE advertise_id=($2)', ['denied', advertise_id])
+    const ad = await (await conexion.query('UPDATE advertises SET status=($1) WHERE advertise_id=($2) RETURNING *', ['denied', advertise_id])).rows[0]
+    await conexion.query('INSERT INTO notifications (owner, message, date) VALUES($1,$2,$3)', [ad.owner, 'We are very sorry your ad was not processed', new Date()])
     return { status: true, content: 'Advertise succesfully approved' }
 }
