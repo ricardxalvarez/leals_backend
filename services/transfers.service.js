3b37be32933641f6493d00467ed836f88ce52cb9
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-11-07 20:10:29 -0400

    changes

--- services/transfers.service.js
+++ services/transfers.service.js
@@ -11,7 +11,7 @@ export async function create_transfer(userid, data) {
     if (transferer_wallet.balance < data.amount) return { statsus: false, content: `You have not enough balance to continue, balance available is ${transferer_wallet.balance}` }
     const destinary = await (await conexion.query('SELECT id FROM usuarios WHERE nombre_usuario=($1)', [data.destinary])).rows[0]
     if (!destinary) return { status: false, content: 'There is no such a user with this username' }
-    const destinary_wallet = await (await conexion.query('SELECT * FROM usuarios WHERE id=($1)', [destinary.id])).rows[0]
+    const destinary_wallet = await (await conexion.query('SELECT * FROM wallets WHERE owner=($1)', [destinary.id])).rows[0]
     if (!destinary_wallet) await create_wallet(destinary.id)
     const destinary_wallet_new_amount = destinary_wallet ? destinary_wallet.balance + data.amount : data.amount
     await conexion.query('UPDATE wallets SET balance=($1) WHERE owner=($2)', [destinary_wallet_new_amount, destinary.id])
