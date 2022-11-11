66f8d0fd8e969e4541bdd524e5bd70ab00186ea5
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2022-11-11 10:42:00 -0400

    changes

--- controllers/advertises.controller.js
+++ controllers/advertises.controller.js
@@ -3,6 +3,8 @@ import { advertisesService } from "../services/index.js";
 export async function post_advertise(req, res, next) {
     const { id } = req.user
     const { post_link } = req.body
+    const valid_posts_links = ['facebook.com', 'https://facebook.com', 'https://instagram.com', 'instagram.com', 'https://tiktok.com', 'tiktok.com', 'https://youtube.com', 'youtube.com']
+    if (!valid_posts_links.some(word => post_link.startsWith(word))) return { status: false, content: 'Please provide a valid url' }
     const response = await advertisesService.post_advertise(id, post_link)
     res.send(response)
 }
