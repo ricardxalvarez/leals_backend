281c1236030ed9bc5ce06ff9b71786e85fce2038
ricardxalvarez <ricardoandresalvarez62341@gmail.com>
2023-01-10 01:20:20 -0400

    businesses v1

--- utils/resize_businesses_images.js
+++ utils/resize_businesses_images.js
@@ -5,7 +5,7 @@ export default async function resize_business_image(business_image) {
     const base = 'base64'
     const base64str = business_image.slice(business_image.indexOf(base) + base.length + 1)
     const buf = Buffer.from(base64str, base);
-    jimp.read(buf, (err, image) => {
+    await jimp.read(buf, (err, image) => {
         if (err || image.bitmap.width === 0 || image.bitmap.height === 0) {
             return result = false
         }
